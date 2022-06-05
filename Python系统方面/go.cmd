5eb39960fb19c692d66a8a1ed2baee712450a5aa
liuhuo23 <45648682+liuhuo23@users.noreply.github.com>
2022-06-05 22:33:47 +0800

    添加 列表，字典，集合 元组 添加 Python 编写windows 服务的模板

---
+++ Python系统方面/go.cmd
@@ -0,0 +1,5 @@
+@echo on
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+
+start python 创建windows服务.py install
